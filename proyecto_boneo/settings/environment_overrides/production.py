ec634ce9e584b429b25e8dabd58df0d89790918f
Alvaro Tuso <alvaro.tuso@adroll.com>
2018-08-12 11:36:51 -0300

    fix env

--- proyecto_boneo/settings/environment_overrides/production.py
+++ proyecto_boneo/settings/environment_overrides/production.py
@@ -3,10 +3,12 @@ from proyecto_boneo.settings.base import *
 DEBUG = False
 TEMPLATE_DEBUG = False
 DATABASES = {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'proyectoboneo',
-    'USER': 'proyectoboneo',
-    'PASSWORD': 'proyectoboneo',
-    'HOST': 'proyecto-boneo.cvttgl8gstqj.us-west-2.rds.amazonaws.com',
-    'PORT': '5432',
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'proyectoboneo',
+        'USER': 'proyectoboneo',
+        'PASSWORD': 'proyectoboneo',
+        'HOST': 'proyecto-boneo.cvttgl8gstqj.us-west-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
 }
